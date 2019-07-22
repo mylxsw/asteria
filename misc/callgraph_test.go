46af3b0bca7581c52e11efcda3a120b83db990a3
管宜尧 <guanyiyao@yunsom.com>
2019-07-22 21:06:02 +0800

    addd unit test

--- misc/callgraph_test.go
+++ misc/callgraph_test.go
@@ -1,18 +1,20 @@
 package misc_test
 
 import (
+	"fmt"
+	"regexp"
 	"testing"
 
 	"github.com/mylxsw/asteria/misc"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCallGraph(t *testing.T) {
 	cg := misc.CallGraph(1)
-	if cg.PackageName != "github.com/mylxsw/asteria/misc_test" {
-		t.Error("test failed")
-	}
 
-	if cg.FuncName != "TestCallGraph" {
-		t.Error("test failed")
-	}
+	fmt.Println(cg)
+
+	assert.Equal(t, "github.com/mylxsw/asteria/misc_test", cg.PackageName)
+	assert.Equal(t, "TestCallGraph", cg.FuncName)
+	assert.Regexp(t, regexp.MustCompile("misc/callgraph_test\\.go$"), cg.FileName)
 }
