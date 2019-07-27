c165f746e97ed72cfe978ac15711dfa16e463bc3
管宜尧 <guanyiyao@yunsom.com>
2019-07-27 18:09:41 +0800

    add more unit test

--- formatter/default_test.go
+++ formatter/default_test.go
@@ -52,3 +52,25 @@ func TestDefaultFormatter_Format(t *testing.T) {
 		assert.Equal(t, f.Format(ts), expected)
 	}
 }
+
+func TestDefaultFormatter_ColorfulFormat(t *testing.T) {
+	now := time.Now()
+
+	var testcases = map[string]event.Event{
+		fmt.Sprintf(`[%s] ` + "\x1b[97;101m[ALER]\x1b[0m test                 Hello, world \x1b[90m{\"#abc\":\"def\",\"uid\":134}\x1b[0m", now.Format(time.RFC3339)): {
+			Time:   now,
+			Module: "test",
+			Level:  level.Alert,
+			Fields: event.Fields{
+				GlobalFields: map[string]interface{}{"abc": "def",},
+				CustomFields: map[string]interface{}{"uid": 134,},
+			},
+			Messages: []interface{}{"Hello, world"},
+		},
+	}
+
+	f := formatter.NewDefaultFormatter(true)
+	for expected, ts := range testcases {
+		assert.Equal(t, expected, f.Format(ts))
+	}
+}
