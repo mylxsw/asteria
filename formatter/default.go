566a3bfa8466c9c597d94e908e53c65937d39f51
管宜尧 <mylxsw@aicode.cc>
2020-11-21 12:28:12 +0800

    add support to strip unicode ctrl characters

--- formatter/default.go
+++ formatter/default.go
@@ -8,11 +8,15 @@ import (
 	"github.com/mylxsw/asteria/color"
 	"github.com/mylxsw/asteria/event"
 	"github.com/mylxsw/asteria/misc"
+	"golang.org/x/text/runes"
+	"golang.org/x/text/transform"
+	"golang.org/x/text/unicode/norm"
 )
 
 // DefaultFormatter 默认日志格式化
 type DefaultFormatter struct {
-	colorful bool
+	colorful                   bool
+	stripCtrlAndExtFromUnicode bool
 }
 
 // NewDefaultFormatter create a new default LogFormatter
@@ -20,16 +24,27 @@ func NewDefaultFormatter(colorful bool) *DefaultFormatter {
 	return &DefaultFormatter{colorful: colorful}
 }
 
+// NewDefaultFormatter create a new default LogFormatter
+func NewDefaultCleanFormatter(colorful bool) *DefaultFormatter {
+	return &DefaultFormatter{colorful: colorful, stripCtrlAndExtFromUnicode: true}
+}
+
 // Event 日志格式化
 func (formatter DefaultFormatter) Format(f event.Event) string {
-	var message string
+	var message, messageBody string
+	if formatter.stripCtrlAndExtFromUnicode {
+		messageBody = StripCtlAndExtFromUnicode(strings.Trim(fmt.Sprint(f.Messages...), "\n"))
+	} else {
+		messageBody = strings.Trim(fmt.Sprint(f.Messages...), "\n")
+	}
+
 	if formatter.colorful {
 		message = fmt.Sprintf(
 			"[%s] %s %-20s %s %s",
 			f.Time.Format(time.RFC3339),
 			misc.ColorfulLevelName(f.Level),
 			misc.ModuleNameAbbr(f.Module),
-			strings.Trim(fmt.Sprint(f.Messages...), "\n"),
+			messageBody,
 			color.TextWrap(color.LightGrey, f.Fields.String()),
 		)
 	} else {
@@ -38,11 +53,28 @@ func (formatter DefaultFormatter) Format(f event.Event) string {
 			f.Time.Format(time.RFC3339),
 			f.Level.GetLevelName(),
 			f.Module,
-			strings.Trim(fmt.Sprint(f.Messages...), "\n"),
+			messageBody,
 			f.Fields.String(),
 		)
 	}
 
 	// 将多行内容增加前缀tab，与第一行内容分开
 	return strings.Trim(strings.Replace(message, "\n", "\n	", -1), "\n	")
+}
+
+// StripCtlAndExtFromUnicode Advanced Unicode normalization and filtering,
+// see http://blog.golang.org/normalization and
+// http://godoc.org/golang.org/x/text/unicode/norm for more
+// details.
+func StripCtlAndExtFromUnicode(str string) string {
+	isOk := func(r rune) bool {
+		return r < 32 || r >= 127
+	}
+	// The isOk filter is such that there is no need to chain to norm.NFC
+	t := transform.Chain(norm.NFKD, runes.Remove(runes.Predicate(isOk)))
+	// This Transformer could also trivially be applied as an io.Reader
+	// or io.Writer filter to automatically do such filtering when reading
+	// or writing data anywhere.
+	str, _, _ = transform.String(t, str)
+	return str
 }
--- go.mod
+++ go.mod
@@ -8,5 +8,6 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/stretchr/testify v1.4.0
+	golang.org/x/text v0.3.4
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
